aa5d8ad9cb4572295178159a98abb8afe5bcd2fd
Marc Juchli <mail@marcjuch.li>
2015-04-22 14:46:39 +0200

    use encode function

--- src/Main.hs
+++ src/Main.hs
@@ -26,17 +26,19 @@ main = do
   putStrLn "Port eingeben"
   portInput <- getLine
   --let port = read portInput ::PortNumber  -- PortNumber does not derive from read
-  connect sock (SockAddrInet 4343 ip)
+  --connect sock (SockAddrInet 4343 ip)
+  connect sock (SockAddrInet 9092 ip)
   putStrLn "ClientId eingeben"
   clientId <- getLine
   putStrLn "TopicName eingeben"
   topicName <- getLine
 
   forever $ do
-    let req = packFtRqMessage $ InputFt (C.pack clientId) (C.pack topicName)
+    let req = encodeRequest 1 0 0 (C.pack clientId) (C.pack topicName)
     sendFtRequest sock req
     forkIO $ do
       input <- SBL.recv sock 4096
+      print input
       let response = readFtResponse input
       print response
     print "consume"
